7bbf0f2f775fe61f341f8e801c4b854d315ace72
Ryan Mulligan <ryanm@ifoodds.com>
2021-06-13 23:25:51 -0700

    Cleaned up and outputting keycodes

--- Kinesis.ino
+++ Kinesis.ino
@@ -1,30 +1,8 @@
 #include <Arduino.h>
 #include <Adafruit_TinyUSB.h> // for Serial
-#include <SoftwareSerial.h>
 #include <Wire.h>
 
-SoftwareSerial softSerial(A0, A1); // RX, TX
-
-char  hidReport[23];                // 00-00-00-00-00-00-00-00
-byte inByte;
-byte byteArray[8];
-int   byteIndex;
-bool  skipSeg = false;
-
 const int  adc_address=41;   // I2C Address
-
-char keyboard_data[80];   // Array to store keyboard values
-
-/**
- * Raw Keycodes
- */
-uint8_t modifier;
-uint8_t key0;
-uint8_t key1;
-uint8_t key2;
-uint8_t key3;
-uint8_t key4;
-uint8_t key5;
 
 void setup()
 {
@@ -36,7 +14,8 @@ void setup()
 
 void loop() // run over and over//
 {
-  int keyboard_chars;
+  char char_count;
+  int key_char;
 
   Wire.beginTransmission(adc_address); // transmit to device
   Wire.write(1);                       // ask how many characters are in buffer
@@ -44,17 +23,17 @@ void loop() // run over and over//
   Wire.requestFrom(adc_address, 1);    // request 1 bytes from slave device
   while(Wire.available())
     {
-      keyboard_chars = Wire.read();
+      char_count = Wire.read();
     }
 
   Wire.beginTransmission(adc_address); // transmit to device
   Wire.write(0);                       // get keyboard characters
   Wire.endTransmission();              // stop transmitting
-  Wire.requestFrom(adc_address, keyboard_chars);    // request characters from slave device
+  Wire.requestFrom(adc_address, char_count);    // request characters from slave device
 
   while(Wire.available())
     {
-      keyboard_chars = Wire.read();
-      Serial.println((int) keyboard_chars);
+      key_char = Wire.read();
+      Serial.println(key_char);
     }
 }
