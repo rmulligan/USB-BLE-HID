da5ef8b6c0f478304499f9e51e1c88cdeaf1af69
Ryan Mulligan <ryanm@ifoodds.com>
2021-06-14 01:05:46 -0700

    Read keypress status pin

--- Kinesis.ino
+++ Kinesis.ino
@@ -2,10 +2,13 @@
 #include <Adafruit_TinyUSB.h> // for Serial
 #include <Wire.h>
 
+#define KEY_PRESS_PIN 7
+
 const int  adc_address=41;   // I2C Address
 
 void setup()
 {
+  pinMode(KEY_PRESS_PIN, INPUT);
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   Wire.begin();
@@ -30,6 +33,8 @@ void loop() // run over and over//
   Wire.write(0);                       // get keyboard characters
   Wire.endTransmission();              // stop transmitting
   Wire.requestFrom(adc_address, char_count);    // request characters from slave device
+
+  Serial.println(digitalRead(KEY_PRESS_PIN));
 
   while(Wire.available())
     {
