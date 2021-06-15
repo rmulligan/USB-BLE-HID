1430bf2aa78b5dd529a202d3e1b7e502c6b4de5b
Ryan Mulligan <ryanm@ifoodds.com>
2021-06-15 00:58:37 -0700

    Blehid working except for 'Windows Key modifier'

--- Kinesis.ino
+++ Kinesis.ino
@@ -1,11 +1,20 @@
 #include <Arduino.h>
 #include <Adafruit_TinyUSB.h> // for Serial
 #include <Wire.h>
+#include <bluefruit.h>
 
 #define KEY_PRESS_PIN 7
 
+/**
+ * Adafruit BLE classes
+ */
+BLEDis bledis;
+BLEHidAdafruit blehid;
+
 const int  adc_address=41;   // I2C Address
 int flag_esc = 0;
+
+bool key_pressed = false;
 
 void setup()
 {
@@ -14,13 +23,25 @@ void setup()
   Serial.begin(9600);
   Wire.begin();
   while ( !Serial ) delay(10);   // for nrf52840 with native usb
+
+  initBluetooth();
+  startAdv();
 }
 
 void loop() // run over and over//
 {
   char char_count;
   int key_char;
-  int key_depressed = digitalRead(KEY_PRESS_PIN);
+  int pressSensor = digitalRead(KEY_PRESS_PIN);
+
+  if (!key_pressed && pressSensor) {
+    // Key Pressed
+    key_pressed = true;
+  } else if (key_pressed && !pressSensor) {
+    // Key Released
+    key_pressed = false;
+    blehid.keyRelease();
+  }
 
   Wire.beginTransmission(adc_address); // transmit to device
   Wire.write(1);                       // ask how many characters are in buffer
@@ -57,18 +78,54 @@ void loop() // run over and over//
 
               // Send Keypress
               //
-              //  ble.keyPress();
-              //
 
-
+              blehid.keyPress(key_char);
               flag_esc=0;
             }
           else
             {
-             // Skip 10 & 13
-
-             // Send keypress
+              // Skip 10 & 13
+              if (key_char != 10 || key_char != 13) {
+                blehid.keyPress(key_char);
+              }
             }
         }
     }
+}
+
+
+void initBluetooth() {
+  Bluefruit.begin();
+  Bluefruit.setName("Kinesis Advantage 2");
+  // Set bluetooth power - Accepted values are: -40, -30, -20, -16, -12, -8, -4, 0, 4
+  Bluefruit.setTxPower(-4);
+  // On-board blue LED: On (true) / Off (false)
+  Bluefruit.autoConnLed(true);
+
+  bledis.setManufacturer("Mike Wu");
+  bledis.setModel("Kinesis Advantage 2");
+
+  bledis.begin();
+  blehid.begin();
+}
+
+// BLE Advertising
+void startAdv()
+{
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_HID_KEYBOARD);
+
+  // Include BLE HID service
+  Bluefruit.Advertising.addService(blehid);
+
+  // There is enough room for the dev name in the advertising packet
+  Bluefruit.Advertising.addName();
+
+  // Apple recommended advertising settings
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
 }
